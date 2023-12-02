38eda9367225971bc1add8f5a795cc4ea3657a1d
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-12-02 06:06:06 -0300

    limpa

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -96,7 +96,7 @@ resource "aws_instance" "web-3" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.web-2.id
   vpc_security_group_ids      = [aws_security_group.web.id]
-  user_data                   = base64encode(data.template_file.user_data.rendered)
+  user_data                   = base64encode(data.template_file.user_data_2.rendered)
 
 }
 
@@ -107,7 +107,7 @@ resource "aws_instance" "web-4" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.web-2.id
   vpc_security_group_ids      = [aws_security_group.web.id]
-  user_data                   = base64encode(data.template_file.user_data.rendered)
+  user_data                   = base64encode(data.template_file.user_data_2.rendered)
 
 }
 
@@ -118,6 +118,10 @@ data "template_file" "user_data" {
 
 }
 
+data "template_file" "user_data" {
+  template = file("./script/user_data2.sh")
+
+}
 
 
 resource "aws_lb" "lb" {
