ce5e03e5223f2eb137b008647b7999a82a4766ff
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-12-02 05:57:24 -0300

    limpa

---
+++ terraform/aws/script/user_data2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+    
+echo "Update with latest packages"
+yum update -y
+    
+echo "Install Apache"
+yum install -y httpd git
+    
+echo "Enable Apache service to start after reboot"
+sudo systemctl enable httpd
+    
+echo "Install application"
+cd /tmp
+git clone https://github.com/NaderSouza/gs-sub
+mkdir /var/www/html
+
+cp /tmp/gs-sub/app/index2.html /var/www/html
+
+# Alteração aqui para selecionar o arquivo certo com base no nome da instância
+# if [[ "$(hostname)" == "web-1" || "$(hostname)" == "web-2" ]]; then
+#   cp /tmp/gs-sub/app/index.html /var/www/html
+# else
+#   cp /tmp/gs-sub/app/index2.html /var/www/html
+# fi
+
+
+echo "Start Apache service"
+service httpd restart
+
